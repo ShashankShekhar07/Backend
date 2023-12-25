9aeb44581987d74eb2049dd2d316c8219bbef667
ShashankShekhar07 <shanky74022@gmail.com>
2023-12-25 23:52:40 +0530

    second commit

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async()=>{
     try{
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MONGODB connected shashank!! DB host: ${connectionInstance.connection.host}`);
+        console.log(`\n MONGODB connected shanky!! DB host: ${connectionInstance.connection.host}`);
     }
     catch(error){
         console.log(`MONGODB connection error`,error);
