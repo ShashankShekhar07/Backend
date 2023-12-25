fea48701a702a6975de3a9d7914c59b874a68677
ShashankShekhar07 <shanky74022@gmail.com>
2023-12-25 23:42:34 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+
+import { DB_NAME } from "./constants";
+
+import express from "express"
+const app = express()
+//TRY CATCH ALWAYS
+// DATABASE IF FAR SO ALWAYS ASYNC AWAIT
+//First method
+// function connectDB(){
+
+// }
+// connectDB()
+
+// Second method
+
+;(async()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",()=>{
+            console.log("ERR:",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on Port ${process.env.PORT}`);
+        })
+    }
+
+    catch(error){
+        console.log("ERROR");
+        throw err
+    }
+})()
+*/
