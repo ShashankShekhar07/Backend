e6c2d8a50c43a064d855faa6ac2eea91d9ff7f5e
ShashankShekhar07 <shanky74022@gmail.com>
2023-12-26 22:03:03 +0530

    models

---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose from "mongoose";
+import jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt"
+const userSchema = new  mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index:true, // For searching purposes 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true, 
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, // cloudinary tool
+        required: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+        }
+    ],
+    password: {
+        type: String,
+        required: [true,"Password is required"],
+    },
+    refreshToken: {
+        type:String,
+    }
+
+
+},{timestamps:true})
+
+userSchema.pre("save",async function (next){
+    if(this.isModified("password")){
+        this.password= bcrypt.hash(this.password,10)
+        next()
+    }
+    else{
+        next()
+    }
+})
+
+userSchema.method.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+} 
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+} 
+
+//// sign with RSA SHA256
+// var privateKey = fs.readFileSync('private.key');
+// var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' });
+
+userSchema.methods.generateRefreshToken = function(){} 
+
+
+export const User= mongoose.model("User",userSchema);
